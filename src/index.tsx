3f75f0443de955493386bc5ccfb6868eedf395c1
Naeem ur Rehman <naeem.ur.rehman.co@gmail.com>
2024-02-02 14:24:57 +0500

    fix: module declaration fix

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from "react";
+import SingleModule from "./pkg/module/SingleModule";
+
+export default (props: any) => <SingleModule
+    {...props}
+/>
